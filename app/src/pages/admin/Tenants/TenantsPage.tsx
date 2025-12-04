b0564203c7bcbf122c6417989b4e58b0e46986f2
Fábio Jesus <fabio_jesus@live.com>
2025-12-04 14:23:12 +0000

    updating tenants page

--- app/src/pages/admin/Tenants/TenantsPage.tsx
+++ app/src/pages/admin/Tenants/TenantsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, Card, HStack, Text, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { FaHouse } from 'react-icons/fa6'
 import PageHeader from '../../../components/PageHeader'
@@ -6,6 +6,8 @@ import { GenericTable } from '../../../components/vc-wallet/Table'
 import { NewButton } from '../../../components/vc-wallet/buttons'
 import { tenantColumns } from './TenantsPage.columns'
 import { tenantMocks } from './TenantsPage.mocks'
+import { FaHome, FaPlus } from 'react-icons/fa'
+import type { ReactElement } from 'react'
 
 export default function TenantsPage() {
   return (
@@ -15,9 +17,12 @@ export default function TenantsPage() {
         title="Tenants"
         description="Manage wallet tenants and related settings."
       />
+      <HStack w="100%" px="4rem" py="2rem">
+        <NewTenantCard/>
+      </HStack>
+
       <VStack w="100%" align="start" px="15%" py="1.5rem" gap={4} color ="black">
         <GenericTable
-          topActions={<Box><Link to="/admin/tenants/new"><NewButton/></Link></Box>}
           columns={tenantColumns}
           data={tenantMocks}
           onView={tenant => console.log('View tenant', tenant.id)}
@@ -27,4 +32,33 @@ export default function TenantsPage() {
       </VStack>
     </VStack>
   )
+}
+
+export function NewTenantCard()
+{
+  return <OperationCard path={"new"} label={"New Tenant"} icon={<FaHouse/>}  
+  />
+}
+
+
+
+export function OperationCard({path, label, description, icon}:{path:string, label:string, description?:string, icon:ReactElement})
+{
+  const Icon = icon;
+  return <Link to={path}>
+        <Card.Root w="10rem" h="8rem" variant="elevated">
+          <Card.Header pb="1rem" fontSize="3rem" alignItems="center" w="100%">
+            <HStack>
+              {Icon}
+            </HStack>
+          </Card.Header>
+          <Card.Footer w="100%" >
+            <Text mx="auto" fontWeight="medium">{label}</Text>
+          </Card.Footer>
+          {description && <Card.Description>
+            {description}
+          </Card.Description>}
+        </Card.Root>
+      </Link>
+      
 }
